3e491bcfd759b0316a9221a180fcb2e1474c445c
AlexT-38 <atiminey@techie.com>
2023-06-19 18:35:07 +0100

    Example JSON output format, issue #7
    Plus minimal python script to load JSON files, for reference.

---
+++ test scripts/json_test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 18 20:25:42 2023
+
+@author: Washu
+"""
+import json
+
+test_file_path = "data/test.json"
+
+with open(test_file_path,"r") as file:
+    
+    
+    # parse x:
+    log = json.load(file)
+    
+    # the result is a Python dictionary:
+    print(log)
